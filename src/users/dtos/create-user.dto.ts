307c33b06c73e8a5707e7730f9e9183cf630fd95
Cecília Nakamura <cecilia_nakamura@mckinsey.com>
2023-02-10 16:11:29 -0300

    DTO - validação

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -2,9 +2,18 @@ import { IsEmail,IsString } from "class-validator";
 
 export class CreateUserDto{
 
+    @IsString()
+    name: string;
+
     @IsEmail()
     email: string;
 
     @IsString()
     password: string;
+
+    @IsString()
+    cpf: string;
+
+    @IsString()
+    cep: string;
 }
