817a99da5cb6ce08ecb964258b7a97f98202227a
Cecília Nakamura <cecilia_nakamura@mckinsey.com>
2023-02-13 12:18:00 -0300

    Substituindo Error -> NotFoundException

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -14,9 +14,12 @@ export class UsersController {
     }
 
     @Get('/:id')
-    findUser(@Param('id') id: string){//toda parte da URL é uma string
-        return this.usersService.findOne(parseInt(id));
-
+    async findUser(@Param('id') id: string){//toda parte da URL é uma string
+        const user = await this.usersService.findOne(parseInt(id));
+        if(!user){
+            throw new NotFoundException('Usuário não encontrado');
+        }
+        return user;
     }
 
     @Get()
