ee5458edf557d9c14a4354475cd2b6efe6b4be10
Cecília Nakamura <cecilia_nakamura@mckinsey.com>
2023-02-14 10:56:44 -0300

    Implementação Cookie Session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session'); //importação incompativel com tsconfig
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['fg321fgj45f$23']
+  }))
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
